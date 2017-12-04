e824e85b463fd98f4591eaca094d0a288ebdca34
ChengYu <858134843@qq.com>
2017-12-04 13:18:06 +0800

    最终稳定版

--- upload/main.py
+++ upload/main.py
@@ -1,20 +1,12 @@
 import os
-import sqlite3
 import time
 
 from .log import Log, view_bar
 from . import config
 from .map_dict import map_dict
-from .read import Read
 from .commit import Commit
-
-
-def to_dict(df):
-    dl = []
-    for row in df.iterrows():
-        d = row[1].to_dict()
-        dl.append(d)
-    return dl
+from .db import SessionManager
+from . import model
 
 
 def run():
@@ -24,45 +16,34 @@ def run():
                  silence_http_multiplier=config.silence_http_multiplier,
                  silence_http_multiplier_max=config.silence_http_multiplier_max,
                  timeout_http=config.timeout_http)
+
     while True:
         if not os.path.exists(config.dbf_cache):
             time.sleep(10)
             continue
         try:
-            r = Read(config.dbf_cache)
-            data = r.select_data(where="status=0", limit=config.max_upload)
-            data = to_dict(data)
+            session = SessionManager().get_session()
+            data = session.query(model.Nqhq).order_by(model.Nqhq.updated_at.desc()).filter(model.Nqhq.status == 0).limit(config.max_upload)
+
             data = map_dict(data,
                             config.map_rule['map'],
                             config.map_rule['strict'],
                             config.map_rule['lower'],
                             swap=config.map_rule['swap'])
-            data = None
-            if data:
+            if data is not None:
                 req.commit_data_list(post_url=config.api_post,
                                      data_list=data,
                                      enable_thread=config.enable_thread,
                                      thread_pool_size=config.thread_pool_size)
 
-                total = r.exe_sql("select count(*) as count from nqhq")
-                uploaded_count = r.exe_sql("select count(*) as count from nqhq WHERE status=1")
-                total = total.ix[0, 0]
-                uploaded_count = uploaded_count.ix[0, 0]
-                view_bar(uploaded_count, total)
+                view_bar(session.query(model.Nqhq).filter(model.Nqhq.status == 1).count(), session.query(model.Nqhq).count())
+                print(session.query(model.Nqhq).filter(model.Nqhq.status == -1).count())
 
             else:
                 # 检测
-                # data = r.select_data(where="status=1 and HQZQDM!='000000'", limit=config.max_upload)
-                # data = to_dict(data)
-                # data = map_dict(data,
-                #                 config.map_rule['map'],
-                #                 config.map_rule['strict'],
-                #                 config.map_rule['lower'],
-                #                 swap=config.map_rule['swap'])
-                # req.verify_data_list(data)
-                # if data:
-                #     pass
                 time.sleep(1)
 
         except Exception as e:
             l.log_error(str(e))
+        finally:
+            session.close()
